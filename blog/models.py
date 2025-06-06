ea287f58c71be35a9f5ed2017044cf1726479fca
sofware-engeneer <kingradomir@gmail.com>
2025-06-06 18:27:06 +0600

    lesson2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# После того когда вы окончательно создали таблицу как указан пример блога ниже
+# Проводим миграции python manage.py makemigrations затем python manage.py migrate
+# После этого заходите в файл admin.py и регистрируете его 
+
+class Blog(models.Model):
+  title = models.CharField(max_length=100, verbose_name='Напишите название блога')
+  image = models.ImageField(upload_to='blog/', verbose_name='Загрузите картинку')
+  description = models.TextField(verbose_name='Дайте описание блога')
+  TYPE_BLOG = (
+    ('IT', 'IT'),
+    ('Business', 'Business'),
+    ('SMM', 'SMM')
+  )
+  type_blog = models.CharField(max_length=100, choices=TYPE_BLOG)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
+  
+  class Meta:
+    verbose_name = 'новость'
+    verbose_name_plural = 'новости'
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from . import models
+
+
+#Вывод всех новостей из модельки Blog но не полностью выводим только название и дату создания поста
+def blog_list_view(request):
+  if request.method == 'GET':
+    blog_list = models.Blog.objects.all().order_by('-id')
+    context = {
+      'blog_list': blog_list,
+    }
+    return render(request, template_name='blog.html', context=context)
+
+#Когда нажимаем кнопку читать подробнее выводит вся информация о данной новости с помощью получения ID
+def blog_detail_view(request, id):
+  if request.method == 'GET':
+    blog_id = get_object_or_404(models.Blog, id=id)
+    context = {
+      'blog_id': blog_id,
+    }
+    return render(request, template_name='blog_detail.html', context=context)
+  
+
+
+
+
 
 def blog(request):
   if request.method == 'GET':
